e8a99d93ac77ad027e35074532db8c6bf7074b6f
Due <williamhenrichdue@gmail.com>
2025-06-27 20:59:58 +0200

    Does this work?

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") {};
+in
+pkgs.mkShell {
+  packages = (with pkgs; [
+    caddy
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-optimize-level-2
+    elmPackages.elm-review
+    elmPackages.elm-test
+    nodejs
+    shellcheck
+  ]);
+
+  shellHook =
+    ''
+    export project="$PWD"
+    export build="$project/.build"
+
+    export PATH="$project/bin:$PATH"
+    '';
+}
